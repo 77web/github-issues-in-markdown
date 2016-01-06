cb81a70765dfc91703b78dcc670ab54eedf59cea
h-hishida <h-hishida@quartetcom.co.jp>
2016-01-06 11:56:25 +0900

    first version

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('hello world');
+$(function(){
+
+  var format_issue_url = function(str){
+    var issueinfo = str.split('#');
+    return '[' + str + '](' + 'https://github.com/' + issueinfo[0] + '/issues/' + issueinfo[1] + ')';
+  };
+
+  $('textarea').on('blur', function(e){
+    var text = $(e.target).val();
+
+    var re = /([\w]+\/[\w]+#[0-9]+)/g;
+    var issues = [];
+    var match = null;
+    while (match = re.exec(text)) {
+      issues.push(match[0]);
+    }
+
+    for (var i = 0; i < issues.length; i++) {
+      text = text.replace(issues[i], format_issue_url(issues[i]));
+    }
+    $(e.target).val(text);
+  });
+});
